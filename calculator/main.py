ea92b0ce9bead4017f129a83757d5cad141b1f5d
Derek Li <derek.j.h.li@gmail.com>
2022-01-02 17:00:07 +0800

    adding the practice code from online course and arranging the buttin like iphone calculator

--- calculator/main.py
+++ calculator/main.py
@@ -1,12 +1,20 @@
+import tkinter as tk
+
 from calculator.utils import Utils
 
 
 def main():
+    window = tk.Tk()
+    window.title('calculator')
+    window.geometry('800x600')
+    window.configure(background = 'white')
+    window.mainloop()
     choose_operator = input("Please choose the operator from +, -, *, / ")
     calculate_data = input("Please give the data list such as 1,2,3,4 : ")
     calculate_math = Utils(calculate_data )
     calculate_math.string_to_num(calculate_data)
     calculate_math.operator_calculate(choose_operator)
+
 
 if __name__ == '__main__':
     main()
